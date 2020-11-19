bc1e89746d60225d841275b6291a23f2c871abe1
Yafira <yafira.martinez@lc.cuny.edu>
2020-11-19 14:28:11 -0500

    added solutions to last week's problems

---
+++ last_week/homework/find-common-chars.py
@@ -0,0 +1,15 @@
+import collections
+from collections import Counter
+
+class Solution(object):
+    def commonChars(self, A):
+        """
+        :type A: List[str]
+        :rtype: List[str]
+        """
+        compare = collections.Counter(A[0])
+
+        for i in A:
+            compare &= collections.Counter(i)
+
+        return list(compare.elements())
